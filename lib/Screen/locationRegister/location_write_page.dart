06643b35be7f5b5bda8cbeae398f07b79e6439fe
byoungchan <byoungchna9321@gmail.com>
2023-11-14 23:30:04 +0900

    장소등록을 클릭했을떄 카테고리 리스트와 장소 입력폼 작성

---
+++ lib/Screen/locationRegister/location_write_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+
+class LocationWritePage extends StatefulWidget {
+  const LocationWritePage({super.key, required this.mapCategoryName});
+  final String mapCategoryName;
+  @override
+  State<LocationWritePage> createState() => _LocationWritePageState();
+}
+
+class _LocationWritePageState extends State<LocationWritePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            // 사진 추가
+
+            // 지명 입력
+            Container(
+              margin: EdgeInsets.only(top: 30.0, right: 10.0, left: 10.0, bottom: 10.0),
+              width: double.infinity,
+              child: TextField(
+                maxLength: 20,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "장소의 이름을 입력하세요!",
+                ),
+                autofocus: true,
+              ),
+            )
+            // 설명창 추가
+          ],
+        ),
+      )
+    );
+  }
+}
--- lib/Screen/location_category.dart
+++ lib/Screen/location_category.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mapdesign_flutter/LocationInfo/modifypage.dart';
+import 'package:mapdesign_flutter/Screen/locationRegister/location_write_page.dart';
 import 'package:mapdesign_flutter/app_colors.dart';
 
 
@@ -13,46 +15,62 @@ class _LocationCategoryState extends State<LocationCategory> {
   @override
   Color iconColor = AppColors.instance.skyBlue;
   Color backgroundColor = AppColors.instance.whiteGrey;
-  final List<Icon> categoryList = [
-    Icon(Icons.menu_book),
-    Icon(Icons.restaurant),
-    Icon(Icons.directions_bike),
-    Icon(Icons.nature_people),
-    Icon(Icons.attractions),
-    Icon(Icons.local_cafe),
-    Icon(Icons.directions_run),
-    Icon(Icons.school),
-    Icon(Icons.local_parking),
-    Icon(Icons.smoking_rooms),
-    Icon(Icons.delete),
-    Icon(Icons.local_convenience_store),
-    Icon(Icons.not_listed_location)
+  final List<String> categoryKey = [
+    "도서관", "음식점", "자전거", "공원", "명소", "카페", "운동", "학교", "주차장", "흡연장", "쓰레기통", "편의점", "기타"
+  ];
+  final List<Map<String, Icon>> categoryList = [
+    {"도서관" : Icon(Icons.menu_book, semanticLabel: "도서관",)},
+    {"음식점" : Icon(Icons.restaurant)},
+    {"자전거" : Icon(Icons.directions_bike)},
+    {"공원" : Icon(Icons.nature_people)},
+    {"명소" : Icon(Icons.attractions)},
+    {"카페" : Icon(Icons.local_cafe)},
+    {"운동" : Icon(Icons.directions_run)},
+    {"학교" : Icon(Icons.school)},
+    {"주차장" : Icon(Icons.local_parking)},
+    {"흡연장" : Icon(Icons.smoking_rooms)},
+    {"쓰레기통" : Icon(Icons.delete)},
+    {"편의점" : Icon(Icons.local_convenience_store)},
+    {"기타" : Icon(Icons.not_listed_location)}
   ];
 
 
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3, // Number of columns
-        crossAxisSpacing: 4.0, // Horizontal space between items
-        mainAxisSpacing: 4.0, // Vertical space between items
-      ),
-      itemCount: categoryList.length,
-      itemBuilder: (BuildContext context, int index) {
-        return GestureDetector(
-          onTap: () {
-            // Handle icon tap
-            print("Icon tapped: ${categoryList[index]}");
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              color: backgroundColor,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: categoryList[index],
-          ),
-        );
-      },
+    return SafeArea(
+      minimum: EdgeInsets.all(10.0),
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4, // Number of columns
+          crossAxisSpacing: 10.0, // Horizontal space between items
+          mainAxisSpacing: 10.0, // Vertical space between items
+        ),
+        itemCount: categoryList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+              decoration: BoxDecoration(
+                color: backgroundColor,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () => {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => LocationWritePage(mapCategoryName: categoryList[index].keys.first) // 글 쓰는 페이지로 이동
+                        )
+                      )
+                    },
+                    icon: categoryList[index].values.first
+                  ),
+                  Text(categoryList[index].keys.first)
+                ],
+              )
+            );
+        },
+      )
     );
   }
 }
